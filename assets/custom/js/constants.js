d1c8171725732134d9aa0beecafc7604868074e5
Arigbede Abiodun <arigbedeabiodun@gmail.com>
2020-07-23 16:04:50 +0100

    refactored code

--- assets/custom/js/constants.js
+++ assets/custom/js/constants.js
@@ -1,3 +1,7 @@
 const BASE_URL = 'https://api.beleful.com.ng/api/';
 
 const LOGIN_URL = BASE_URL + 'user/auth/'
+
+const VENDOR_CATEGORY = BASE_URL + 'vendors/category/'
+
+const access_token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk1NTE5NTkwLCJqdGkiOiJhMTFjZjYyNGZhZjA0MjE3YjViNjcwNzVjMWQ3ZTk1MSIsInVzZXJfaWQiOiI0ZDgyZGFjMS1lYmE3LTRkYjItYmY1NC00NzNiNzU0NWRiMWIifQ.e5j2MdPKDeIC3qIqBT7rmAPgrE3GPVgMKZmTXPX3od0`
