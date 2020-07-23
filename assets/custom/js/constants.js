73961c2a8a395cc4620d0a46995cb25a9ae69fa4
Eyinjuoluwa <barbieadol@gmail.com>
2020-07-23 16:21:31 +0100

    product list done

--- assets/custom/js/constants.js
+++ assets/custom/js/constants.js
@@ -4,4 +4,4 @@ const LOGIN_URL = BASE_URL + 'user/auth/'
 
 const VENDOR_CATEGORY = BASE_URL + 'vendors/category/'
 
-const access_token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk1NjAyMjIxLCJqdGkiOiJjZjZiZThkZDI1OTA0YzY5ODQ3MWUwYjNiODJjYzZjMSIsInVzZXJfaWQiOiI0ZDgyZGFjMS1lYmE3LTRkYjItYmY1NC00NzNiNzU0NWRiMWIifQ.GorEd4Xmz7ehyHjxK4n_fI5ntcFlmzlan5uDj9CVvMw`
+const PRODUCT_LIST_URL = BASE_URL + 'vendors/items/'
