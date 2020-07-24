248264dbc3d58566dea3a24330f1cc2910737c3c
Arigbede Abiodun <arigbedeabiodun@gmail.com>
2020-07-24 14:02:18 +0100

    add product category

--- assets/custom/js/constants.js
+++ assets/custom/js/constants.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://api.beleful.com.ng/api/';
+const BASE_URL = 'https://staging-api.beleful.com.ng/api/';
 
 const LOGIN_URL = BASE_URL + 'user/auth/'
 
 const VENDOR_CATEGORY = BASE_URL + 'vendors/category/'
 
-const access_token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk1NTE5NTkwLCJqdGkiOiJhMTFjZjYyNGZhZjA0MjE3YjViNjcwNzVjMWQ3ZTk1MSIsInVzZXJfaWQiOiI0ZDgyZGFjMS1lYmE3LTRkYjItYmY1NC00NzNiNzU0NWRiMWIifQ.e5j2MdPKDeIC3qIqBT7rmAPgrE3GPVgMKZmTXPX3od0`
+const access_token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk1NjAyMjIxLCJqdGkiOiJjZjZiZThkZDI1OTA0YzY5ODQ3MWUwYjNiODJjYzZjMSIsInVzZXJfaWQiOiI0ZDgyZGFjMS1lYmE3LTRkYjItYmY1NC00NzNiNzU0NWRiMWIifQ.GorEd4Xmz7ehyHjxK4n_fI5ntcFlmzlan5uDj9CVvMw`
