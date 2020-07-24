d51d6553aa1d5087a0d34ffb90a27c6e98a48995
Eyinjuoluwa <barbieadol@gmail.com>
2020-07-24 15:53:33 +0100

    updating product now working

--- assets/custom/js/constants.js
+++ assets/custom/js/constants.js
@@ -1,7 +1,16 @@
 const BASE_URL = 'http://127.0.0.1:8000/api/';
 
-const LOGIN_URL = BASE_URL + 'user/auth/'
+const LOGIN_URL = 'user/auth/';
 
-const VENDOR_CATEGORY = BASE_URL + 'vendors/category/'
+const VENDOR_CATEGORY = 'vendors/category/';
 
-const PRODUCT_LIST_URL = BASE_URL + 'vendors/items/'
+const PRODUCT_URL = 'vendors/items/';
+
+function generateURL(SUB_URL){
+    URL = BASE_URL + SUB_URL;
+    console.log(URL);
+    if(URL.endsWith('/')){
+        return URL;
+    }
+    return URL + '/';
+};
