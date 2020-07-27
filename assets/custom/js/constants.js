993e1277a1685f24adf5d18a5dbe7e5c11d8eab7
Sotunde Abiodun <abiodun.sotunde@firstpavitech.com>
2020-07-27 11:17:19 +0100

    fix base url

--- assets/custom/js/constants.js
+++ assets/custom/js/constants.js
@@ -1,5 +1,5 @@
-//const BASE_URL = 'https:staging-api.beleful.com.ng/api/';
-const BASE_URL = ' http://127.0.0.1:8000/api/';
+const BASE_URL = 'https:staging-api.beleful.com.ng/api/';
+//const BASE_URL = ' http://127.0.0.1:8000/api/';
 
 const LOGIN_URL = 'user/auth/';
 
