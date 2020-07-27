69a967a075265d0152eee403609c786e9b17ceb8
Sotunde Abiodun <abiodun.sotunde@firstpavitech.com>
2020-07-27 11:06:54 +0100

    fixes made now ready as MVP

--- assets/custom/js/constants.js
+++ assets/custom/js/constants.js
@@ -1,20 +1,34 @@
-const BASE_URL = 'https:staging-api.beleful.com.ng/api/';
+//const BASE_URL = 'https:staging-api.beleful.com.ng/api/';
+const BASE_URL = ' http://127.0.0.1:8000/api/';
 
 const LOGIN_URL = 'user/auth/';
 
 const VENDOR_CATEGORY = 'vendors/category/';
 
-const ACCESS__TOKEN = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk1NjQyNzEzLCJqdGkiOiIyM2ZlMmQwZDEzMGU0NDcyYmJiOGVhYTgwMWRkYzhmMSIsInVzZXJfaWQiOiI0ZDgyZGFjMS1lYmE3LTRkYjItYmY1NC00NzNiNzU0NWRiMWIifQ.ZSRcxx74xaWOOCsm4Sxg-JUwMK9SRxdoH4Kk8G0TDKM`
-
 const PRODUCT_URL = 'vendors/items/';
 
-const UPDATE__CATEGORY = 'vendors/category/'
+const UPDATE_CATEGORY = 'vendors/category/';
+
+const DELIVERY_URL = 'order/web/';
+
+const DELIVERY_DETAIL_URL = 'order/';
+
+const DASHBOARD_URL = 'vendors/dashboard/';
 
 function generateURL(SUB_URL){
     URL = BASE_URL + SUB_URL;
-    // console.log(URL);
+    console.log(URL);
     if(URL.endsWith('/')){
         return URL;
     }
     return URL + '/';
 };
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
